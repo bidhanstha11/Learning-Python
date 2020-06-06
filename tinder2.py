7040ba06fcc06e32a32de76fcf844b937d121671
bidhanstha11 <57820444+bidhanstha11@users.noreply.github.com>
2020-06-06 19:19:30 +0545

    Add files via upload

---
+++ tinder2.py
@@ -0,0 +1,10 @@
+#tinder part 2
+girls = ["Kajal", "sunny", "namrata", "saili", "Vidya"]
+my_choice = ["sunny", "kajal", "saili"]
+
+for each_girl in girls:
+    if each_girl in my_choice:
+        print("This is it for you today and your girl name is ", each_girl)
+
+    else:
+        print(f"not this one {each_girl}")
---
+++ tinderanime.py
@@ -0,0 +1,9 @@
+boys = ["Luffy", "Ace", "Naruto", "Ichigo", "Boruto", "Sasuke"]
+worst_characeter_nowadays = ["Boruto", "Sasuke"]
+
+for boy in boys:
+    if boy in worst_characeter_nowadays:
+        print(f"The worst character now is {boy}")
+
+    else:
+        print(f"{boy} isn't useless")
